a945346efa294e53b8c1f482412a37dec013311c
Maksim Shilov <storm25132513@gmail.com>
2020-11-09 11:25:58 +0300

    Updates

---
+++ precache-manifest.4f9b07f773294d5feeeb9a9df281d762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e495a27a49969fd98ae3bfa5c5713f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "703aca5c0a66251f6f20",
+    "url": "./static/css/main.1ec6b295.chunk.css"
+  },
+  {
+    "revision": "746361c03644bd53c840",
+    "url": "./static/js/2.28ceeb5c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.28ceeb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703aca5c0a66251f6f20",
+    "url": "./static/js/main.3f4d1bb8.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.3f4d1bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ff2beacfd7eaf04b74",
+    "url": "./static/js/runtime-main.fd7c83d2.js"
+  }
+]);
